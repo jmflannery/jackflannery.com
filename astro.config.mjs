7d9a02c08a7cf79127ca80d5da53a97659052948
Jack Flannery <hello@jackflannery.com>
2025-01-27 15:52:19 -0500

    Updates:
    * update astro and astrojs/tailwind
    * update tsconfig
    * update astro config
    * update tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  experimental: {
+    svg: true,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    'container',
+    'mx-auto',
+    'w-screen',
+    'h-screen',
+    'bg-gradient-to-b',
+    'bg-gradient-to-t',
+    'from-sky-500',
+    'to-indigo-500',
+    'bg-linear-to-bl',
+    'from-violet-500',
+    'to-fuchsia-500',
+    'h-px',
+    'h-96',
+    'h-full',
+    'px-1',
+    'px-2',
+    'px-4',
+    'px-6',
+    'md:px-2',
+    'md:px-4',
+    'md:px-2',
+    'xl:px-6',
+  ],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
